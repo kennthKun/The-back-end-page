c9589b8056f4113268ed9c8d951fb92bddd57f60
kennethKun <610589930@qq.com>
2017-11-29 13:07:57 +0800

    后端页面响应式

---
+++ js/nav.js
@@ -0,0 +1,45 @@
+var navs = [{
+	"title" : "后台首页",
+	"icon" : "icon-computer",
+	"href" : "page/main.html",
+	"spread" : false
+},{
+	"title" : "列表1",
+	"icon" : "icon-text",
+	"href" : "page/news/newsList.html",
+	"spread" : false
+},{
+	"title" : "列表2",
+	"icon" : "icon-text",
+	"href" : "page/links/linksList.html",
+	"spread" : false
+},{
+	"title" : "404页面",
+	"icon" : "&#xe61c;",
+	"href" : "page/404.html",
+	"spread" : false
+},{
+	"title" : "列表3",
+	"icon" : "&#xe631;",
+	"href" : "page/systemParameter/systemParameter.html",
+	"spread" : false
+},{
+	"title" : "二级菜单演示",
+	"icon" : "&#xe61c;",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "二级菜单1",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "二级菜单2",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+}]
